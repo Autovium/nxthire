602c217f707a24bc8f44ade01b2f6fc7822bb452
Sandun Viduranga <admin@autovium.com>
2026-03-28 23:36:18 +0530

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -1,22 +1,12 @@
 const openJobs = [
     {
-        id: "Test",
-        title: "Test Job Title",
-        company: "Test Company",
-        location: "Test Location",
-        type: "Full-time",
-        salary: "$120k - $160k",
-        image: "Ads/senior-frontend-engineer.jpg",
-        applyLink: "https://docs.google.com/forms/your-google-form-link-1" 
-    },
-    {
-        id: "test2",
-        title: "test job title 2",
-        company: "test company 2",
-        location: "San Francisco, CA",
-        type: "Full-time",
-        salary: "$140k - $180k",
-        image: "https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&auto=format&fit=crop&w=1200&q=80",
-        applyLink: "https://docs.google.com/forms/your-google-form-link-4"
+        title: "Automation Engineer",
+        company: "Autovium Technologies",
+        location: "Handapangoda",
+        type: "Full time",
+        salary: "100k-200k",
+        image: "Ads/2443d478-cce3-4a46-8d40-34df4f902e01.jpg",
+        applyLink: "https://forms.gle/xABZEBgo61nQtYhv8",
+        id: "job_20260328_233508"
     }
 ];
