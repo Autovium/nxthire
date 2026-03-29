614c8c3deef41a4bfd4dbca38f613bcd5c75c80b
Sandun Viduranga <admin@autovium.com>
2026-03-29 13:01:56 +0530

    add job

--- jobs.js
+++ jobs.js
@@ -18,5 +18,15 @@ const openJobs = [
         image: "Ads/senior-frontend-engineer.jpg",
         applyLink: "https://forms.gle/xABZEBgo61nQtYhv8",
         id: "job_20260328_234040"
+    },
+    {
+        title: "1",
+        company: "1",
+        location: "2",
+        type: "1",
+        salary: "1",
+        image: "Ads/2443d478-cce3-4a46-8d40-34df4f902e01.jpg",
+        applyLink: "",
+        id: "job_20260329_130001"
     }
 ];
